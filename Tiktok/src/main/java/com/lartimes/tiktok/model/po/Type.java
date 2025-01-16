fd244d86de319a3e5ceedb695d5eb12a17eec66f
Lartimes <lartimes2004@163.com>
2025-01-16 21:32:09 +0800

    nothing: test connection

---
+++ Tiktok/src/main/java/com/lartimes/tiktok/model/po/Type.java
@@ -0,0 +1,49 @@
+package com.lartimes.tiktok.model.po;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author lartimes
+ */
+@Data
+@TableName("type")
+public class Type implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Boolean open;
+
+    private String icon;
+
+    private Integer sort;
+
+    private String labelNames;
+
+    @TableField("is_deleted")
+    private Boolean deleted;
+
+    private LocalDateTime gmtCreated;
+
+    private LocalDateTime gmtUpdated;
+
+
+}
